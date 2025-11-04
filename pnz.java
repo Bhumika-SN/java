6cb9eff0aefffbf1b6c114f26bfc489764806fef
Bhumika SN <bhumikasnn.dev@gmail.com>
2025-11-04 11:21:17 +0530

    Add files via upload

---
+++ pnz.java
@@ -0,0 +1,20 @@
+public class pnz {
+    public static void mian (String [] args)
+    {
+        int num = 9;
+
+        if (num > 0)
+        { 
+            System.out.println(num +"postive");
+        }
+        else if (num < 0)
+        {
+            System.out.println( num + "nagative");
+
+        }
+        else {
+            System.out.println(num + " is zero");
+        }
+    }
+    
+}
