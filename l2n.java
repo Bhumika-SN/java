0714c76c5ce9c6f92c77f6d6a9b7518bea5a6894
Bhumika SN <bhumikasnn.dev@gmail.com>
2025-11-04 11:43:23 +0530

    Add files via upload

---
+++ l2n.java
@@ -0,0 +1,20 @@
+public class l2n {
+    public static void main ( String [] args)
+    {
+        int num1 = 9;
+        int num2 = 10;
+         if (num1 > num2)
+         {
+            System.out.println(num1 + " is larger");
+
+         } 
+         else if ( num2 > num1)
+          {
+            System.out.println(num2 + " is larger");
+          }
+          else 
+          {
+            System.out.println( " both are equal");
+          }
+    }
+}
