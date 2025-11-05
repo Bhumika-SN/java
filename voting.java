880e4de677b6d94cfd02d45584f3ded0ba221204
Bhumika SN <bhumikasnn.dev@gmail.com>
2025-11-05 14:36:08 +0530

    Add files via upload

---
+++ voting.java
@@ -0,0 +1,18 @@
+public class voting {
+     public static void main ( String [] args )
+     {
+         int age = 17 ;
+
+         if ( age >= 18)
+         {
+            System.out.println (" youare elegible to vote " );
+
+         }
+          else if ( age< 18 )
+          {
+            System.out.println 
+            ( " your are minor ");
+          }
+     }
+    
+}
