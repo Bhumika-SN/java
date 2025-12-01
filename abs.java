2dfcd06f84d237effc5a38eb7119daaf888b345f
Bhumika SN <bhumikasnn.dev@gmail.com>
2025-12-01 20:32:41 +0530

    Add files via upload

---
+++ abs.java
@@ -0,0 +1,19 @@
+public class abs {
+    public static void main ( String [] args)
+    {
+        int num = 100;
+        int absvalue;
+
+        if (num < 0)
+        {
+           absvalue = -num;
+        }
+        else 
+        {
+           absvalue = num;
+        }
+        System.out.println(" absolute value of " + num +" is " + absvalue  );
+    }
+    
+    
+}
