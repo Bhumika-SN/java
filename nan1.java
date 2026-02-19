bda092461c451086889b861423c521f403780ebb
Boomika SN <bhumikasnn.dev@gmail.com>
2026-02-19 22:49:53 +0530

    Add files via upload

---
+++ nan1.java
@@ -0,0 +1,23 @@
+public class nan1 {
+    public static String reverseIfPalindrome(String s) {
+
+        String reversed = "";
+
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed = reversed + s.charAt(i);
+        }
+
+        
+        if (s.equals(reversed)) {
+            return reversed;
+        } else {
+            return s;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseIfPalindrome("madam"));
+        System.out.println(reverseIfPalindrome("hello"));
+    }
+}
