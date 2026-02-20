02b9720a482961df4606acd207c650124fc5593f
Boomika SN <bhumikasnn.dev@gmail.com>
2026-02-20 12:29:15 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Coustomer {
+    private int id;
+    private String name;
+
+    public Coustomer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+      return "ID: " + id + ", Name: " + name;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    
+        List<Coustomer> list = new ArrayList<>();
+
+        list.add(new Coustomer(1, "Boomika"));
+        list.add(new Coustomer(2, "Charan"));
+        list.add(new Coustomer(3, "Appu"));
+
+        System.out.println(list);
+    }
+}
