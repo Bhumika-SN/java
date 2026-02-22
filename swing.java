ef9510f86246c9365168c674bcb67906b3717fdb
Boomika SN <bhumikasnn.dev@gmail.com>
2026-02-22 22:43:21 +0530

    Add files via upload

---
+++ swing.java
@@ -0,0 +1,15 @@
+public class swing {
+    public static void main (String []args){
+
+
+    
+
+
+
+    int x = 7;
+if (x % 2 == 0) {
+    System.out.println("Even");
+} else {
+    System.out.println("Odd");
+}
+    }}
