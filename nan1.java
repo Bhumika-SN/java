4eb7796379a3eb8338a24d029d225aec89b1e48e
Boomika SN <bhumikasnn.dev@gmail.com>
2026-03-28 23:23:20 +0530

    Upload nan1.java

--- nan1.java
+++ nan1.java
@@ -15,7 +15,6 @@ public class nan1 {
             return s;
         }
     }
-
     public static void main(String[] args) {
         System.out.println(reverseIfPalindrome("madam"));
         System.out.println(reverseIfPalindrome("hello"));
